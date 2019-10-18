94c01905b562dfc6e5cd254b04fbeb26d35bef6b
Min Su Kim <kim97minsu@gmail.com>
2019-10-17 23:34:01 -0700

    Commit 9 - Created loops.js file.

---
+++ resume_page/app.js
@@ -0,0 +1,37 @@
+for(var i = 0; i <= 99; i++){
+   console.log(i);
+}
+
+for(var i = 99; i >= 0; i--){
+   console.log(i);
+}
+
+for(var i = 298; i >= 1; i-=3){
+   console.log(i);
+}
+
+for (var i = 0; i <= 99; i += 1) {
+	if(i % 2 === 0) {
+		console.log(i);
+	}
+}
+
+for (var i = 98; i >= 0; i-=1) {
+	if(i % 2 !== 0) {
+		console.log(i);
+	}
+}
+
+for(var i = 49; i <= 72; i++){
+   console.log(i);
+}
+
+for(var i = 81; i >= 26; i--){
+   console.log(i);
+}
+
+for (var i = 0; i <= 91; i += 1) {
+	if(i % 3 === 0) {
+		console.log(i);
+	}
+}
